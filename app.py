e9436caee9766db7d5d4ac2570c199ffa899247f
Semo <tpah20@gmail.com>
2022-07-20 22:30:45 +0900

    s3_key 변경

--- app.py
+++ app.py
@@ -249,19 +249,17 @@ if __name__ == "__main__":
     # global model download
     model, latest_gl_model_v = model_download()
 
+    # 새로 생성되는 글로벌 모델 버전
+    next_gl_model = latest_gl_model_v + 1
+
+
     # server_status 주소
     inform_SE: str = 'http://10.152.183.18:8000/FLSe/'
-
-    # server_status 확인 => 전 global model 버전
-    # server_res = requests.get(inform_SE + 'info')
-    # latest_gl_model_v = int(server_res.json()['Server_Status']['GL_Model_V'])
-    
-    next_gl_model = latest_gl_model_v + 1
 
     inform_Payload = {
             # 형식
             'S3_bucket': 'fl-flower-model', # 버킷명
-            'S3_key': 'model_V%s.h5'%latest_gl_model_v,  # 모델 가중치 파일 이름
+            'S3_key': 'gl_model_%s_V.h5'%latest_gl_model_v,  # 모델 가중치 파일 이름
             'play_datetime': today_time, # server 수행 시간
             'FLSeReady': True, # server 준비 상태 on
             'GL_Model_V' : latest_gl_model_v # GL 모델 버전
