8ad393a519e9a2341544600b1d8e6cff94028cbd
Semo <tpah20@gmail.com>
2022-10-12 02:40:54 +0900

    .

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ logger = logging.getLogger(__name__)
 
 # FL 하이퍼파라미터 설정
 class FL_server_parameter:
-    num_rounds = 30
-    local_epochs = 10
+    num_rounds = 50
+    local_epochs = 20
     batch_size = 1024
     val_steps = 10
     latest_gl_model_v = 0 # 이전 글로벌 모델 버전
