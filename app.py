1f231ac70336beeebc25774ea365229f29bf6b8c
Semo <tpah20@gmail.com>
2023-03-15 19:44:55 +0900

    update

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ logger = logging.getLogger(__name__)
 
 # FL 하이퍼파라미터 설정
 class FL_server:
-    num_rounds = 20
-    local_epochs = 10
+    num_rounds = 5
+    local_epochs = 3
     batch_size = 32
     val_steps = 32
     latest_gl_model_v = 0 # 이전 글로벌 모델 버전
